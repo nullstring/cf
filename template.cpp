9305eed6b67bd942eba9a1398a40549937613cf6
Harsh <harsh.version.1@gmail.com>
2016-04-17 19:42:43 -0400

    init

--- template.cpp
+++ template.cpp
@@ -2,8 +2,12 @@
 #include <math.h>
 
 using namespace std;
+
 typedef long long INT;
+typedef unsigned long long U_INT;
+// double precision: 14
 typedef double FLOAT;
+typedef long double DOUBLE;
 
 /*
 cout.precision(10);
